160bea7db2098b26dfc13feeff879e22455effc0
Иван Сафронов <safroniv@mail.ru>
2022-07-17 16:25:42 +0300

    GB - Алгоритмы. Урок 4 Задание 2 двоичное дерево. ready.

--- JobLessonAlgo04v01Part02/Program.cs
+++ JobLessonAlgo04v01Part02/Program.cs
@@ -1,105 +1,29 @@
 ﻿
-Console.WriteLine("Hello, World!");
+var tree = new Tree();
+tree.AddItem(50);
+tree.AddItem(33);
+tree.AddItem(12);
+tree.AddItem(14);
+tree.AddItem(13);
+tree.AddItem(5);
+tree.AddItem(6);
+tree.AddItem(4);
+tree.AddItem(15);
+tree.AddItem(33);
+tree.AddItem(67);
+tree.AddItem(66);
+tree.AddItem(65);
+tree.AddItem(64);
+tree.AddItem(69);
+tree.AddItem(68);
+tree.AddItem(71);
+tree.AddItem(73);
+tree.AddItem(75);
+tree.AddItem(63);
+tree.AddItem(16);
 
-public class TreeNode
-{
-    public int Value { get; set; }
-    public TreeNode? LeftChild { get; set; }
-    public TreeNode? RightChild { get; set; }
-    public override bool Equals(object obj)
-    {
-        var node = obj as TreeNode;
-        if (node == null)
-            return false;
-        return node.Value == Value;
-    }
+tree.RemoveItem(73);
+tree.PrintTree();
 
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(Value, LeftChild, RightChild);
-    }
-}
-public interface ITree
-{
-    TreeNode GetRoot();
-    void AddItem(int value); // добавить узел
-    void RemoveItem(int value); // удалить узел по значению
-    TreeNode GetNodeByValue(int value); //получить узел дерева по значению
-    void PrintTree(); //вывести дерево в консоль
-}
-public static class TreeHelper
-{
-    public static NodeInfo[] GetTreeInLine(ITree tree)
-    {
-        var bufer = new Queue<NodeInfo>();
-        var returnArray = new List<NodeInfo>();
-        var root = new NodeInfo() { Node = tree.GetRoot() };
-        bufer.Enqueue(root);
-        while (bufer.Count != 0)
-        {
-            var element = bufer.Dequeue();
-            returnArray.Add(element);
-            var depth = element.Depth + 1;
-            if (element.Node.LeftChild != null)
-            {
-                var left = new NodeInfo()
-                {
-                    Node = element.Node.LeftChild,
-                    Depth = depth,
-                };
-                bufer.Enqueue(left);
-            }
-            if (element.Node.RightChild != null)
-            {
-                var right = new NodeInfo()
-                {
-                    Node = element.Node.RightChild,
-                    Depth = depth,
-                };
-                bufer.Enqueue(right);
-            }
-        }
-        return returnArray.ToArray();
-    }
-}
-public class NodeInfo
-{
-    public int Depth { get; set; }
-    public TreeNode? Node { get; set; }
-}
 
-public class Tree : ITree
-{
-    private TreeNode? _root;
 
-    public TreeNode Root
-    {
-        get { return _root; }
-        set { _root = value; }
-    }
-    
-    public void AddItem(int value)
-    {
-        throw new NotImplementedException();
-    }
-
-    public TreeNode GetNodeByValue(int value)
-    {
-        throw new NotImplementedException();
-    }
-
-    public TreeNode GetRoot()
-    {
-        return Root;
-    }
-
-    public void PrintTree()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void RemoveItem(int value)
-    {
-        throw new NotImplementedException();
-    }
-}
---
+++ JobLessonAlgo04v01Part02/TreeNode.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+public class TreeNode
+{
+    public int Value { get; set; }
+    public TreeNode? LeftChild { get; set; }
+    public TreeNode? RightChild { get; set; }
+    public TreeNode(int value)
+    {
+        Value = value;
+    }
+    public override bool Equals(object obj)
+    {
+        var node = obj as TreeNode;
+        if (node == null)
+            return false;
+        return node.Value == Value;
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Value, LeftChild, RightChild);
+    }
+    public int DateCompare(int date)
+    {
+        return Value.CompareTo(date);
+    }
+}
+
+
